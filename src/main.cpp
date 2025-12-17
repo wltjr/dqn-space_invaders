ad10e78044b39f0c8efad7557c23a0c1e5061653
William L. Thomson Jr. <w@wltjr.com>
2025-12-17 10:14:16 -0500

    src/main.cpp: Switch to external dqn

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@
 #include <torch/torch.h>
 
 #include "argp.hpp"
+#include "networks.hpp"
 #include "replay_memory.hpp"
 #include "state_history.hpp"
 
@@ -26,48 +27,6 @@ const int WIDTH = 160;
 const int CROP_X = 13; // (110 - 84) / 2
 const int CROP_HEIGHT = 84;
 const int CROP_WIDTH = 110;
-
-struct NetImpl : torch::nn::Module
-{
-    NetImpl(int64_t frames, int64_t actions)
-        : conv1(torch::nn::Conv2dOptions(frames, 32, 8).stride(4)), //  N , 4 x 8
-          conv2(torch::nn::Conv2dOptions(32, 64, 4).stride(2)), // 32 , 8 x 8
-          conv3(torch::nn::Conv2dOptions(64, 64, 3).stride(1)), // 64 , 4 x 4
-          fc1(3136, 512), // 64 x 7 x 7
-          fc2(512, actions)
-    {
-        register_module("conv1", conv1);
-        register_module("conv2", conv2);
-        register_module("conv3", conv3);
-        register_module("fc1", fc1);
-        register_module("fc2", fc2);
-    }
-
-    torch::Tensor forward(torch::Tensor x)
-    {
-        x = torch::relu(conv1(x));
-        x = torch::relu(conv2(x));
-        x = torch::relu(conv3(x));
-        x = torch::flatten(x,1);
-        x = torch::relu(fc1(x));
-        return fc2(x);
-    }
-
-    torch::Tensor act(torch::Tensor state)
-    {
-        torch::Tensor q_value = forward(state);
-        torch::Tensor action = std::get<1>(q_value.max(1));
-        return action;
-    }
-
-    torch::nn::Conv2d conv1;
-    torch::nn::Conv2d conv2;
-    torch::nn::Conv2d conv3;
-    torch::nn::Linear fc1{nullptr};
-    torch::nn::Linear fc2{nullptr};
-};
-
-TORCH_MODULE(Net);
 
 /**
  * @brief Copy policy model network to target model network
@@ -258,7 +217,7 @@ void init_nn_modules(torch::nn::Module const &model, int init_weights)
  */
 void train(args &args, 
            ale::ALEInterface &ale,
-           std::shared_ptr<NetImpl>  model,
+           std::shared_ptr<DQNImpl>  model,
            torch::Device &device)
 {
     int update;
@@ -266,7 +225,7 @@ void train(args &args,
     ale::reward_t max_score;
     int total_steps;
     ReplayMemory memory(args.memory);
-    NetImpl policy(args.history_size, ACTIONS);
+    DQNImpl policy(args.history_size, ACTIONS);
     torch::optim::Adam optimizer(policy.parameters(),
                                  torch::optim::AdamOptions(args.alpha));
 
@@ -560,7 +519,7 @@ void train(args &args,
 int main(int argc, char* argv[])
 {
     struct args args;
-    std::shared_ptr<NetImpl> model;
+    std::shared_ptr<DQNImpl> model;
 
     // parse command line options
     argp_parse (&argp, argc, argv, 0, 0, &args);
@@ -591,7 +550,7 @@ int main(int argc, char* argv[])
     if(torch::cuda::is_available())
         device = torch::Device(torch::kCUDA);
 
-    model = std::make_shared<NetImpl>(args.history_size, ACTIONS);
+    model = std::make_shared<DQNImpl>(args.history_size, ACTIONS);
 
     // load model
     if(args.load)
