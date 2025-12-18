6b1438336e9a82d5cfd7540dfd5b7157087d9360
William L. Thomson Jr. <w@wltjr.com>
2025-12-18 10:04:42 -0500

    src/networks.hpp: Initial structure for dueling dqn

--- src/networks.hpp
+++ src/networks.hpp
@@ -41,3 +41,46 @@ struct DQNImpl : torch::nn::Module
 };
 
 TORCH_MODULE(DQN);
+
+/**
+ * @brief Dueling Deep Q-Network
+ */
+struct DuelingDQNImpl : torch::nn::Module
+{
+    int64_t actions;
+    torch::nn::Conv2d conv1;
+    torch::nn::Conv2d conv2;
+    torch::nn::Conv2d conv3;
+    torch::nn::Linear fc1_adv{nullptr};
+    torch::nn::Linear fc1_val{nullptr};
+    torch::nn::Linear fc2_adv{nullptr};
+    torch::nn::Linear fc2_val{nullptr};
+
+    /**
+     * @brief Construct a new DuelingDQNImpl object
+     *
+     * @param frames number of stacked frames (not layers) per state tensor
+     * @param actions number of actions available
+     */
+    DuelingDQNImpl(int64_t frames, int64_t actions);
+
+    /**
+     * @brief Perform forward pass of input data through network
+     *
+     * @param x tensor of states up to history size, shape: (bs,f,h,w)
+     *
+     * @return torch::Tensor q-values from the network, shape: (bs,actions)
+     */
+    torch::Tensor forward(torch::Tensor x);
+
+    /**
+     * @brief Get action from network
+     *
+     * @param state tensor of current state, shape: (bs,f,h,w)
+     *
+     * @return torch::Tensor scalar of the action to be taken
+     */
+    torch::Tensor act(torch::Tensor state);
+};
+
+TORCH_MODULE(DuelingDQN);
